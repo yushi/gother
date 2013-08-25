5c38a140f6bec965352b1834125921bb34fa430d
Yushi Nakai <yushi.nakai@gmail.com>
2013-08-25 22:58:44 +0900

    support version

--- gother.go
+++ gother.go
@@ -8,14 +8,21 @@ import (
 	"net/http"
 )
 
+const VERSION = "0.0.1-dev"
+
 func hello_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello Go!")
 }
 
 func main() {
 	port := flag.Int("p", 9090, "listen port")
+	printVersion := flag.Bool("v", false, "print version")
 	flag.Parse()
 
+	if *printVersion {
+		fmt.Println(VERSION)
+		return
+	}
 	procHandler := new(handler.ProcHandler)
 	procHandler.Start()
 	http.HandleFunc("/hello", hello_handler)
